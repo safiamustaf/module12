998f7406ecd14fcabcb078b58a78a7fa4e58866a
FIRST_NAME LAST_NAME <safiamustaf>
2023-05-27 04:56:51 -0500

    pushing

---
+++ db/db.js
@@ -0,0 +1,10 @@
+const connection = require("./connection");
+
+class DBA {
+  // Keeping a reference to the connection on the class in case we need it later
+  constructor(connection) {
+    this.connection = connection;
+  }
+}
+
+module.exports = new DB(connection);
