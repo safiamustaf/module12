91152fe9d23198da3ae671a1498f4d8a62087a44
FIRST_NAME LAST_NAME <safiamustaf>
2023-05-28 05:03:19 -0500

    first commit

--- index.js
+++ index.js
@@ -2,3 +2,39 @@ const { prompt } = require("inquirer");
 const db = require("./db/db");
 
 
+function topLevelMenu() {
+    prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [
+                {
+                    name: "View All Departments",
+                    value: "VIEW_DEPARTMENTS"
+                }
+            
+            ]}]).then(resp => {
+                switch (resp.choice) {
+                    case "VIEW_DEPARTMENTS":
+                        ViewAllDepartments();
+                        break;
+                    default:
+                        quitProgram();
+                }
+            })
+        }
+
+
+        function viewAllDepartments() {
+            db.getAllDepartmentsFromDb()
+              .then(([departments]) => {
+                console.log("\n");
+                console.table(departments);
+              })
+              .then(() => topLevelMenu());
+          }
+
+          function quitTheProgram() {
+            process.exit();
+          }
